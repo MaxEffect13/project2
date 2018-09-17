331bababb62f82048d16260d87039b667b7c3e4a
davidcalkins <david.calkins.1000@gmail.com>
2018-09-17 15:59:45 -0400

    Pulling JSON from api is working

--- PullFromApi/src/main/java/com/revature/drivers/PullJsonFromApi.java
+++ PullFromApi/src/main/java/com/revature/drivers/PullJsonFromApi.java
@@ -1,16 +1,28 @@
 package com.revature.drivers;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PullJsonFromApi {
+	
+	
+	/** The location to save the resulting json to. */
+	private static String OUTPUT_FILE = 
+			"C:\\Users\\David\\Desktop\\Dev Environments\\gitStuff"
+			+ "\\Revature\\project2\\heroes.json";
+	
+	
+	
 	
 	/** The base URL to Request from. */
 	public static final String BASE_URL = "http://www.superheroapi.com/api.php/";
@@ -19,23 +31,126 @@ public class PullJsonFromApi {
 	 * coding this for now as it was a free key. */
 	public static final String API_KEY = "104846317149136";
 	
+	/** Returns the string that begins the parameter list, if applicable. */
+	public static final String PARAMETER_PREFIX = "/";
+	
 	/** The parameters for the search. Uses a question mark where a parameter 
 	 * should be replaced at runtime. */
-	public static final String PARAMETERS = "/?";
+	public static final String PARAMETERS = "?";
 	
 	/** The maximum number of retries that getJSON will make when attempting 
 	 * to connect to the API. */
 	private static final int MAX_RETRIES = 3;
 	
 	
+	
+	
+	/** The prefix of the json file. */
+	private static final String JSON_PREFIX = "{\"heroes\": [";
+	
+	/** The suffix of the json file. */
+	private static final String JSON_SUFFIX = "]}";
+	
+	/** The name of the response field in the json. Used to tell if the 
+	 * request was successful. */
+	private static final String RESPONSE_TEXT = "response";
+	
+	/** The response value from the API upon a successful request. */
+	private static final String HERO_SUCCESS = "success";
+	
+	
 	public static void main(String[] args) {
+		
+		// A string that holds the response from the api. 
+		String jsonString;
+		
+		// A hash map constructed from the json.
+		HashMap jsonMap;
+		
+		// The file for the resulting hero json
+		File outFile = new File(OUTPUT_FILE);
+		
+		// The writer for the output file. 
+		BufferedWriter writer = null;
+		
+		// True until the first hero is processed. Used to separate heroes by comma
+		boolean firstHero = true;
+		
+		
 		try {
-			getRequest(BASE_URL + API_KEY + "/1");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			// Attempt to create a new file, or not if it already exists. 
+			outFile.createNewFile();
+			
+			// Open a writer to the file. Fails if file wasn't created, which is
+			// handled by the exception handler. 
+			writer = new BufferedWriter(new FileWriter(outFile));
+			
+			// Write the json prefix
+			writer.write(JSON_PREFIX);
+			
+			ObjectMapper om = new ObjectMapper();
+			
+			int idCounter = 1;
+			
+			// While we have not encountered an invalid id, keep requesting heros
+			// and saving them. Or until we encounter an exception. 
+			while (true) {
+				// Get the raw json string, and convert it to a map to read the values
+				jsonString = getHeroJSON(idCounter++);
+				jsonMap = om.readValue(jsonString, HashMap.class);
+				
+				// If the response isn't a successful one, we're likely done. 
+				if (!jsonMap.get(RESPONSE_TEXT).equals(HERO_SUCCESS)) {
+					System.out.println("Response: " + jsonMap.get(RESPONSE_TEXT));
+					break;
+				}
+				
+				// If this isn't the first hero that was written, put a comma to 
+				// separate the json entries. 
+				if (!firstHero) {
+					writer.write(",");
+				}
+				firstHero = false;
+				
+				// Attempt to write the JSON to the file. 
+				writer.write(jsonString);
+				
+			}
+		
+		} catch(FileNotFoundException e) {
+			// Print the problem to the console if there was a problem 
+			// opening the writer. 
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// Whether success or error, attempt to write the JSON suffix to the 
+			// end of the file, and close it. 
+			try {writer.write(JSON_SUFFIX);} catch(IOException e) {
+				System.err.println("Failed to write JSON suffix. ");
+			}
+			try {writer.close();} catch(IOException e) {
+				System.err.println("Failed to close JSON file.");
+			}
 		}
+		
 	} // end of main
+	
+	
+	/**
+	 * Takes an id of a hero, and returns a string representing the JSON of the 
+	 * requested hero. 
+	 * @param heroId - The id of the hero to get. 
+	 * @return A string representation of the result of the query. 
+	 * @throws IOException - If there was a problem making the request or 
+	 * or getting a response. 
+	 */
+	public static String getHeroJSON(int heroId) throws IOException{
+		String heroURL = BASE_URL + API_KEY + PARAMETER_PREFIX 
+												+ String.valueOf(heroId);
+		return getRequest(heroURL);
+	} // end of getHeroJSON
+	
 	
 	
 	/**
@@ -63,7 +178,7 @@ public class PullJsonFromApi {
 		String inputLine = null;
 		
 		try {
-			// TODO: Put witin loop to retry on failed connection. 
+			// TODO: Put within loop to retry on failed connection. 
 			// Attempt to parse the urlString. It's possible it might fail due 
 			// to an MalformedURLException. However, it can also throw a 
 			// generic RuntimeException on certain URL parameters, which needs 
