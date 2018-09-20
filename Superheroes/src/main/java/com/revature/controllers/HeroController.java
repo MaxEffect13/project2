1ab919c936bffa8876049e411359557d07d14928
davidcalkins <david.calkins.1000@gmail.com>
2018-09-20 13:09:59 -0400

    Slight change

--- Superheroes/src/main/java/com/revature/controllers/HeroController.java
+++ Superheroes/src/main/java/com/revature/controllers/HeroController.java
@@ -23,4 +23,6 @@ public class HeroController {
 		return null;
 	}
 	
+	
+	
 }
--- Superheroes/src/main/java/com/revature/controllers/TeamController.java
+++ Superheroes/src/main/java/com/revature/controllers/TeamController.java
@@ -1,7 +1,18 @@
 package com.revature.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TeamController {
 	
+	
+	@GetMapping("/team/create")
+	@ResponseBody
+	public String getTeamCreate() {
+		return null;
+	}
 	
 	
 }
